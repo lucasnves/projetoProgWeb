0cd6517eca86c75ecca2b8821b55f87d2b1a8ccc
Lucas Neves <lucas.snowsg@gmail.com>
2024-08-13 19:34:46 -0300

    all on store

--- frontend/src/lib/store.js
+++ frontend/src/lib/store.js
@@ -1,9 +1,10 @@
 import api from '../api';
 
 // Carregar todos os filmes
-export const loadAllMovies = async () => {
+// Type: movie, series, documentary
+export const loadAllWorks = async (type) => {
   try {
-    const response = await api.get('api/works/?type=movie');
+    const response = await api.get('api/works/?type=' + type);
     return response.data;
   } catch (error) {
     console.error('Erro ao buscar filmes:', error);
@@ -12,7 +13,7 @@ export const loadAllMovies = async () => {
 };
 
 // Carregar um filme específico
-export const loadMovie = async (id) => {
+export const load = async (id) => {
   try {
     const response = await api.get(`api/works/?id=${id}`);
     return response.data[0];
@@ -23,9 +24,10 @@ export const loadMovie = async (id) => {
 };
 
 // Filtrar filmes
-export const filterMovies = async (filter, searchTerm = '') => {
+// Type: movie, series, documentary
+export const filterWorks = async (filter, type, searchTerm = '') => {
   try {
-    let url = 'api/works/filter/?filter=' + filter + '&type=movie';
+    let url = 'api/works/filter/?filter=' + filter + '&type=' + type;
     if (filter === 'name' && searchTerm.trim() !== '') {
       url += '&name=' + encodeURIComponent(searchTerm);
     }
@@ -38,12 +40,64 @@ export const filterMovies = async (filter, searchTerm = '') => {
 };
 
 // Carregar avaliações de um filme
-export const loadMovieRatings = async (id) => {
+export const loadRatings = async (id) => {
   try {
     const response = await api.get(`api/ratings/?work_id=${id}`);
     return response.data;
   } catch (error) {
     console.error('Erro ao buscar ratings:', error);
     return [];
+  }
+};
+
+/*
+ * const movie = {
+ *  name: "Novo Filme",
+ *  description: "Descrição do novo filme",
+ *  author: "Autor Exemplo",
+ *  work_created: 2024,
+ *  genres: [1, 2], // IDs dos gêneros
+ *  box_office: 20
+ *  year_released: 2010
+ * };
+ * 
+ * const series = {
+ *  name: "Novo Filme",
+ *  description: "Descrição do novo filme",
+ *  author: "Autor Exemplo",
+ *  work_created: 2024,
+ *  genres: [1, 2], // IDs dos gêneros
+ *  seasons: 4,
+ *  episodes: 230,
+ *  year_started: 2010,
+ *  year_ended: 2020,
+ * };
+ * 
+ * const documentary = {
+ *  name: "Novo Filme",
+ *  description: "Descrição do novo filme",
+ *  author: "Autor Exemplo",
+ *  work_created: 2024,
+ *  genres: [1, 2], // IDs dos gêneros
+ *  duration: 4,
+ *  country_of_origin: "Brasil",
+ *  theme: "Terror",
+ * };
+ */
+
+// type: movie, series, documentary
+// Adicionar uma nova obra
+export const createWork = async (data, type) => {
+  try {
+    const requestData = { ...data, work_type: type };
+    const response = await api.post(`api/works/`, requestData, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error('Erro ao criar obra:', error);
+    return null;
   }
 };
